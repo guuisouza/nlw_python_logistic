e1b0ae66d5c2a6887f3bbbbec9904ec17edea47b
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-02-09 14:43:51 -0300

    feat: Implementing business rules for tag creator view

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,18 +1,20 @@
 # View para ciração de tags - self -> contexto da classe
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
 class TagCreatorView:
     '''
         responsability for interacting with HTTP
     '''
     # method
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica de regra de negcio
-        print("Estou na minha view")
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
         # retorno do http
-        return HttpResponse(status_code=200, body={"hello": "world"})
-        
+        return HttpResponse(status_code=200, body=formatted_response)
